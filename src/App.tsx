28e309e4b87604ed472c035cf3d13441c3eafccb
ananyaa <dk80655@gmail.com>
2026-06-03 23:40:45 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        hello
+      </h1>
+    </div>
+  )
+}
+export default App
